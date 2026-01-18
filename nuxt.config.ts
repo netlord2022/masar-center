8781a00c84d3e3d72dafae0130e3f8a2c67228da
netlord2022 <netlord2022@gmail.com>
2026-01-18 20:15:52 +0100

    trigger

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   //compatibilityDate: "2025-07-15",
   devtools: { enabled: process.env.NODE_ENV !== "production" },
-
   // SSG Configuration
   ssr: true,
   nitro: {
