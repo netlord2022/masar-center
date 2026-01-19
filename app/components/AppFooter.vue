043c9eadbb30019deff5d0a4d6ec1324318a1371
netlord2022 <netlord2022@gmail.com>
2026-01-20 00:58:15 +0100

    design footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,22 +1,38 @@
 <template>
-  <footer role="contentinfo">
-    <div class="bg-dark dark:bg-white pt-6 pb-20">
-      <div
-        class="w-full flex justify-center flex-wrap py-4 text-white dark:text-black font-medium px-4 sm:px-6 lg:px-8 gap-4"
+  <footer role="contentinfo" class="mx-auto px-6 w-full mb-8">
+    <div
+      class="bg-dark dark:bg-white pt-6 pb-8 rounded-t-lg px-8 gap-4 flex items-center"
+    >
+      <a
+        href="https://www.facebook.com/profile.php?id=61585719966946"
+        target="_blank"
+        title="Masar facebook"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          class="w-6 h-6 fill-white dark:fill-black"
+        >
+          <path
+            d="M576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 440 146.7 540.8 258.2 568.5L258.2 398.2L205.4 398.2L205.4 320L258.2 320L258.2 286.3C258.2 199.2 297.6 158.8 383.2 158.8C399.4 158.8 427.4 162 438.9 165.2L438.9 236C432.9 235.4 422.4 235 409.3 235C367.3 235 351.1 250.9 351.1 292.2L351.1 320L434.7 320L420.3 398.2L351 398.2L351 574.1C477.8 558.8 576 450.9 576 320z"
+          />
+        </svg>
+        <span class="sr-only">Masar facebook</span></a
       >
-        <NuxtLinkLocale to="privacy">{{
-          $t("privacy.shortTitle")
-        }}</NuxtLinkLocale>
-        <NuxtLinkLocale to="impressum" class="">{{
-          $t("impressum.title")
-        }}</NuxtLinkLocale>
-      </div>
-      <div
-        class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8 text-center text-sm text-white dark:text-black"
-      >
-        &copy; {{ new Date().getFullYear() }} {{ $t("copyRight") }} - Images by
-        Freepik
-      </div>
+      <a
+        href="https://www.instagram.com/masar.ug.de"
+        target="_blank"
+        title="Masar instagram"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 640"
+          class="w-6 h-6 fill-white dark:fill-black"
+        >
+          <path
+            d="M320.3 205C256.8 204.8 205.2 256.2 205 319.7C204.8 383.2 256.2 434.8 319.7 435C383.2 435.2 434.8 383.8 435 320.3C435.2 256.8 383.8 205.2 320.3 205zM319.7 245.4C360.9 245.2 394.4 278.5 394.6 319.7C394.8 360.9 361.5 394.4 320.3 394.6C279.1 394.8 245.6 361.5 245.4 320.3C245.2 279.1 278.5 245.6 319.7 245.4zM413.1 200.3C413.1 185.5 425.1 173.5 439.9 173.5C454.7 173.5 466.7 185.5 466.7 200.3C466.7 215.1 454.7 227.1 439.9 227.1C425.1 227.1 413.1 215.1 413.1 200.3zM542.8 227.5C541.1 191.6 532.9 159.8 506.6 133.6C480.4 107.4 448.6 99.2 412.7 97.4C375.7 95.3 264.8 95.3 227.8 97.4C192 99.1 160.2 107.3 133.9 133.5C107.6 159.7 99.5 191.5 97.7 227.4C95.6 264.4 95.6 375.3 97.7 412.3C99.4 448.2 107.6 480 133.9 506.2C160.2 532.4 191.9 540.6 227.8 542.4C264.8 544.5 375.7 544.5 412.7 542.4C448.6 540.7 480.4 532.5 506.6 506.2C532.8 480 541 448.2 542.8 412.3C544.9 375.3 544.9 264.5 542.8 227.5zM495 452C487.2 471.6 472.1 486.7 452.4 494.6C422.9 506.3 352.9 503.6 320.3 503.6C287.7 503.6 217.6 506.2 188.2 494.6C168.6 486.8 153.5 471.7 145.6 452C133.9 422.5 136.6 352.5 136.6 319.9C136.6 287.3 134 217.2 145.6 187.8C153.4 168.2 168.5 153.1 188.2 145.2C217.7 133.5 287.7 136.2 320.3 136.2C352.9 136.2 423 133.6 452.4 145.2C472 153 487.1 168.1 495 187.8C506.7 217.3 504 287.3 504 319.9C504 352.5 506.7 422.6 495 452z"
+          />
+        </svg>
+        <span class="sr-only">Masar instagram</span>
+      </a>
     </div>
 
     <div
@@ -74,6 +90,33 @@
         </div>
       </div>
     </div>
+    <div
+      class="w-full bg-black dark:bg-white relative z-1 rounded-b-lg ronded-t-none mb-24 mx-auto"
+    >
+      <div
+        class="footer-section footer-section-bottom px-10 w-full max-w-[1386px] overflow-hidden relative z-2 mx-auto my-0 dark:text-white"
+      >
+        <div
+          class="footer-columns-row bg-white dark:bg-black flex rounded-t-2xl relative min-h-12 justify-between items-center px-8"
+          dir="ltr"
+        >
+          <div class="footer-column footer-copyrights-container">
+            &copy; {{ new Date().getFullYear() }} {{ $t("copyRight") }} - Images
+            by Freepik
+          </div>
+          <div class="footer-column footer-additional-menu-container">
+            <nav class="gap-4 flex">
+              <NuxtLinkLocale to="privacy">{{
+                $t("privacy.shortTitle")
+              }}</NuxtLinkLocale>
+              <NuxtLinkLocale to="impressum" class="">{{
+                $t("impressum.title")
+              }}</NuxtLinkLocale>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 <style scoped>
@@ -86,5 +129,27 @@
 }
 .link-box-item img {
   @apply min-w-6 w-6 transition-all duration-400;
+}
+.footer-columns-row::before,
+.footer-columns-row::after {
+  box-shadow: 0 20px 0 0 #ffffff;
+  content: "";
+  position: absolute;
+  background-color: transparent;
+  bottom: 0;
+  height: 40px;
+  width: 20px;
+}
+.footer-columns-row::after {
+  left: 100%;
+  border-bottom-left-radius: 20px;
+}
+.footer-columns-row::before {
+  right: 100%;
+  border-bottom-right-radius: 20px;
+}
+html.dark .footer-columns-row::before,
+html.dark .footer-columns-row::after {
+  box-shadow: 0 20px 0 0 #010617 !important;
 }
 </style>
