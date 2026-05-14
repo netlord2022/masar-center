7a7b7ff1acd410e2e4c245d3c2f6c31771440b5b
netlord2022 <netlord2022@gmail.com>
2026-05-14 02:50:19 +0200

    fix: routers ignor og

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      ignore: ["/.netlify/images"],
+      ignore: ["/.netlify/images", "/_og"],
       routes: [
         "/",
         "/de",
