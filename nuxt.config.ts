8d35172dc99cfbd74acae9b2b0afa426e50ef42d
netlord2022 <netlord2022@gmail.com>
2026-05-17 01:20:40 +0200

    fix: update for netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -97,12 +97,12 @@ export default defineNuxtConfig({
     preview: {
       dev: true, // enables the floating editor button locally
     },
-    repository: {
-      provider: "github",
-      owner: "netlord2022",
-      repo: "masar-center",
-      branch: "feature/blog",
-    },
+    // repository: {
+    //   provider: "github",
+    //   owner: "netlord2022",
+    //   repo: "masar-center",
+    //   branch: "feature/blog",
+    // },
   },
   app: {
     head: {
