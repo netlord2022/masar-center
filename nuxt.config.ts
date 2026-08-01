8a71d97ae378d0d7418d4c0f07ccb51c8cdfc0ac
netlord2022 <netlord2022@gmail.com>
2026-08-01 22:07:03 +0200

    feat: upgrade deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -162,7 +162,7 @@ export default defineNuxtConfig({
   },
 
   // CSS and assets
-  css: ["./app/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
